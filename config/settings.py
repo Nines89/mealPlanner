53cbc56862c14c5a5cdee694b8f903451461432c
paolo <p.zanghi89@gmail.com>
2026-06-12 11:19:00 +0200

    first css tallwind approach

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']   # file statici del progetto
+STATIC_ROOT = BASE_DIR / 'staticfiles'     # destinazione per collectstatic (produzione)
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
